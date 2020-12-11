98027d4c455c5f525118556214b1ce8e7e13171b
刘新辉 <liunewshine@qq.com>
2020-12-11 15:01:28 +0800

    修复短链接显示bug

--- src/views/appList/AppList.vue
+++ src/views/appList/AppList.vue
@@ -31,7 +31,7 @@
       <table>
         <tr>
           <td>短链接:</td>
-          <td>http://fir.sout.fun/{{item.currentVersion.shortCode}}</td>
+          <td>{{getUrl(item)}}</td>
         </tr>
         <tr>
           <td>PackageName:</td>
@@ -116,111 +116,114 @@
       gotoPreview(item) {
         window.open("/" + item.shortCode, "_blank");
       },
+      getUrl(item) {
+        return window.location.protocol + "//" + window.location.host + "/" + item.shortCode;
+      },
     },
   };
 </script>
 
 <style lang="scss" scoped>
-  @import "../../style/variables";
+@import "../../style/variables";
 
-  .main-home {
-    font-size: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    width: 1020px;
-    margin: 0 auto;
-    padding: 20px 0;
+.main-home {
+  font-size: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 1020px;
+  margin: 0 auto;
+  padding: 20px 0;
 
-    .card {
-      margin: 12px;
-      width: 30.5%;
-      height: 430px;
-      background-color: #fff;
-      padding: 0;
+  .card {
+    margin: 12px;
+    width: 30.5%;
+    height: 430px;
+    background-color: #fff;
+    padding: 0;
+    transition: all .25s;
+
+    i {
       transition: all .25s;
-
-      i {
-        transition: all .25s;
-      }
-
-      &:hover {
-        transform: translateY(-4px);
-        box-shadow: 0 15px 30px rgba(0, 0, 0, .1);
-
-        .app-name {
-          color: #4a4a4a;
-        }
-
-        i {
-          transform: scale(1.2);
-        }
-      }
     }
 
-    .upload {
-      background-color: $color-primary;
-      cursor: pointer;
-
-      i {
-        color: white;
-        font-size: 85px;
-        margin-bottom: 25px;
-      }
-
-      .upload-text {
-        color: white;
-        font-size: 17px;
-        margin-top: 135px;
-      }
-    }
-
-    .app {
-      position: relative;
-      box-sizing: border-box;
-      padding: 50px 45px 45px 45px;
+    &:hover {
+      transform: translateY(-4px);
+      box-shadow: 0 15px 30px rgba(0, 0, 0, .1);
 
       .app-name {
-        font-size: 18px;
-        margin: 25px 0;
+        color: #4a4a4a;
       }
 
-      table {
-        font-size: 12px;
-
-        td {
-          padding: 4px 0;
-        }
-
-        td:last-child {
-          padding-left: 10px;
-          color: #1A1A1A
-        }
-      }
-
-      .app-btn {
-        text-align: center;
-        margin: 60px auto 0;
+      i {
+        transform: scale(1.2);
       }
     }
   }
+
+  .upload {
+    background-color: $color-primary;
+    cursor: pointer;
+
+    i {
+      color: white;
+      font-size: 85px;
+      margin-bottom: 25px;
+    }
+
+    .upload-text {
+      color: white;
+      font-size: 17px;
+      margin-top: 135px;
+    }
+  }
+
+  .app {
+    position: relative;
+    box-sizing: border-box;
+    padding: 50px 45px 45px 45px;
+
+    .app-name {
+      font-size: 18px;
+      margin: 25px 0;
+    }
+
+    table {
+      font-size: 12px;
+
+      td {
+        padding: 4px 0;
+      }
+
+      td:last-child {
+        padding-left: 10px;
+        color: #1A1A1A
+      }
+    }
+
+    .app-btn {
+      text-align: center;
+      margin: 60px auto 0;
+    }
+  }
+}
 </style>
 <style lang="scss">
-  .main-home {
-    .card {
-      .el-upload, .el-upload-dragger {
-        width: 100%;
-        height: 100%;
-      }
+.main-home {
+  .card {
+    .el-upload, .el-upload-dragger {
+      width: 100%;
+      height: 100%;
+    }
 
-      .el-upload-dragger {
-        background-color: transparent;
-        border: none;
-      }
+    .el-upload-dragger {
+      background-color: transparent;
+      border: none;
+    }
 
-      .el-upload-dragger.is-dragover {
-        background-color: rgba(0, 0, 0, 0.3);
-        border-radius: 0;
-      }
+    .el-upload-dragger.is-dragover {
+      background-color: rgba(0, 0, 0, 0.3);
+      border-radius: 0;
     }
   }
+}
 </style>
--- src/views/main/HeadBar.vue
+++ src/views/main/HeadBar.vue
@@ -10,7 +10,7 @@
       </div>
       <div style="flex: 1"/>
       <div class="avatar">
-        <img src="http://avatar-file.flow.ci/avatar/8cd0609795fe169c5210477837fcdfa1.png?r=PG" alt="头像">
+        <div class="photo"/>
         <span class="name">{{username}}</span>
         <span class="logout" @click="logout">注销</span>
       </div>
@@ -55,68 +55,69 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "../../style/variables";
+@import "../../style/variables";
 
-  .header-wrapper {
-    border-bottom: 1px solid rgba(208, 208, 208, .3);
-  }
+.header-wrapper {
+  border-bottom: 1px solid rgba(208, 208, 208, .3);
+}
 
-  .header {
+.header {
+  @include gravityCenter;
+  height: $header-height;
+  width: 1024px;
+  margin: 0 auto;
+
+  .header-nav {
     @include gravityCenter;
-    height: $header-height;
-    width: 1024px;
-    margin: 0 auto;
+    color: #9b9b9b;
+    font-size: 20px;
 
-    .header-nav {
-      @include gravityCenter;
-      color: #9b9b9b;
-      font-size: 20px;
-
-      i {
-        margin: 0 10px;
-        color: #c6c6c6;
-      }
-    }
-
-    .avatar {
-      position: relative;
-      width: 155px;
-      height: 80px;
-
-      img {
-        width: 48px;
-        height: 48px;
-        border-radius: 50%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        margin: auto;
-      }
-
-      span {
-        display: inline-block;
-        width: 90px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        word-wrap: normal;
-        line-height: 30px;
-        position: absolute;
-        right: 0;
-      }
-
-      .name {
-        color: #4a4a4a;
-        top: 13px;
-      }
-
-      .logout {
-        font-size: 12px;
-        color: #9b9b9b;
-        bottom: 13px;
-        cursor: pointer;
-      }
+    i {
+      margin: 0 10px;
+      color: #c6c6c6;
     }
   }
+
+  .avatar {
+    position: relative;
+    width: 155px;
+    height: 80px;
+
+    .photo {
+      width: 45px;
+      height: 45px;
+      border-radius: 5px;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      margin: auto;
+      background-color: #f7b92d;
+    }
+
+    span {
+      display: inline-block;
+      width: 90px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      word-wrap: normal;
+      line-height: 30px;
+      position: absolute;
+      right: 0;
+    }
+
+    .name {
+      color: #4a4a4a;
+      top: 13px;
+    }
+
+    .logout {
+      font-size: 12px;
+      color: #9b9b9b;
+      bottom: 13px;
+      cursor: pointer;
+    }
+  }
+}
 </style>
